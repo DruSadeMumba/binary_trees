0a3c0abdcd79f8f850c8c08336d88dd87c70b5b4
Dru Mumba <drusademumba@gmail.com>
2023-11-01 21:26:02 +0300

    Write a function that counts the leaves in a binary tree

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,1 +1,20 @@
 #include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - counts the leaves in a binary tree
+ * @tree: pointer to the root node
+ * Return: count
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	unsigned long count;
+
+	if (!tree)
+		return (0);
+
+	if (!tree->left && !tree->right)
+		return (1);
+
+	count = binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
+	return (count);
+}
