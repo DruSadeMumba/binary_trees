88dac44e91f91ef55fa98c3fabb37d9b4434119c
Celse13 <rugiracelse13@gmail.com>
2023-11-01 02:51:56 -0700

    Checking if there is a lead if binary tree

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,1 +1,15 @@
 #include "binary_trees.h"
+#include "binary_trees.h"
+/**
+ * binary_tree_is_leaf -checking if there are descendants
+ * @node: pointer to the node to track
+ * Return: 1 when children are there or 0 when not
+**/
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (!node)
+		return (0);
+	if (!node->left && node->right)
+		return (1);
+	return (0);
+}
