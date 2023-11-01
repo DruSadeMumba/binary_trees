edceadd1a7bb3117343ebdef00f537bc9bb4f07d
Dru Mumba <drusademumba@gmail.com>
2023-11-01 22:49:25 +0300

    Write a function that checks if a binary tree is perfect

--- binary_trees.h
+++ binary_trees.h
@@ -70,4 +70,7 @@ heap_t *array_to_heap(int *array, size_t size);
 int heap_extract(heap_t **root);
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
 
+int is_perfect_recursive(const binary_tree_t *tree, int depth, int count);
+
+
 #endif
