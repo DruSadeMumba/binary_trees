ea0b2c91eae3e0f324ed979edb67640416b4fb47
Celse13 <rugiracelse13@gmail.com>
2023-11-01 14:42:51 -0700

    verify if were on the root of the binary tree

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,1 +1,12 @@
-#include "binary_trees.h"
+#include "ibinary_trees.h"
+/**
+ * binary_tree_is_root - verify if we're on the root of the binary tree
+ * @node: pointer to the current node
+ * Return: 1 if we're on the root or 0 if not
+ **/
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (!node && !node->parent)
+		return (0);
+	return (1);
+}
